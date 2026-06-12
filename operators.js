ae815cd5984a74d9c212f021e8c58b75f1a5365e
vishalsb25csc-crypto <vishalsb25csc@vetias.ac.in>
2026-06-12 11:18:58 +0530

    added operators

--- operators.js
+++ operators.js
@@ -5,6 +5,14 @@ console.log(a%b);
 console.log(a*b);
 console.log(a/b);
 console.log(a**b);
+c=10
+console.log(a=c+10);
+console.log(a=c-10);
+console.log(a=c/10);
+console.log(a=c%10);
+console.log(a=c**10);
+
+
 
 
 
