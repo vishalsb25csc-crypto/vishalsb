20174488b67a13ddd53e5d91ca134d13e8531cf9
vishalsb25csc-crypto <vishalsb25csc@vetias.ac.in>
2026-06-12 11:37:04 +0530

    added operators

--- operators.js
+++ operators.js
@@ -6,11 +6,20 @@ console.log(a*b);
 console.log(a/b);
 console.log(a**b);
 c=10
-console.log(a=c+10);
-console.log(a=c-10);
-console.log(a=c/10);
-console.log(a=c%10);
-console.log(a=c**10);
+console.log(a+=c);
+console.log(a-=c);
+console.log(a/=c);
+console.log(a%=c);
+console.log(a**=c);
+var a=10
+var b=20
+console.log("comparison operator");
+console.log(a==b);
+console.log(a!=b);
+console.log(a<=b);
+console.log(a>=b);
+
+
 
 
 
