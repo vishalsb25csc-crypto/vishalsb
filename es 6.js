56300314e6e1af3eb836f0ca1e94454beff0c3ad
vishalsb25csc-crypto <vishalsb25csc@vetias.ac.in>
2026-06-15 11:38:01 +0530

    added es 6.js

---
+++ es 6.js
@@ -0,0 +1,25 @@
+let studentcount =10;
+studentcount = 11;
+console.log("student count":,studentcount);
+//const
+const collegeName = "VETIAS";
+console.log("college name:",collegename);
+//template literals
+let studentname = "arun";
+console.log(`welcome $(studentName) to ${collegename}`);
+//object
+const student = {
+    name:"arun",
+    age:20,
+    department:CS
+}
+//destructuring
+const{name,age,department}=student;
+console.log("Name:",name);
+console.log("age",age);
+console.log("department:",department);
+//arrow function
+const displayStudent = () => {
+    console.log(`student Name: ${name}`);
+    };
+
