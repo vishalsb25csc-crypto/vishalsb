4631881c10f8aea89bc1d66a8076277613105dfd
vishalsb25csc-crypto <vishalsb25csc@vetias.ac.in>
2026-06-15 11:52:40 +0530

    added es 6.js

--- es 6.js
+++ es 6.js
@@ -1,25 +1,46 @@
-let studentcount =10;
+//let
+let studentcount = 11;
 studentcount = 11;
-console.log("student count":,studentcount);
+console.log("student count:", studentcount);
 //const
-const collegeName = "VETIAS";
-console.log("college name:",collegename);
-//template literals
-let studentname = "arun";
-console.log(`welcome $(studentName) to ${collegename}`);
+const collageName = "VETIAS";
+console.log("collage Name:",collagename);
+//Template literals
+console.log('Welcome ${studentName} to ${collage Name}');
 //object
-const student = {
-    name:"arun",
-    age:20,
-    department:CS
-}
-//destructuring
-const{name,age,department}=student;
-console.log("Name:",name);
-console.log("age",age);
-console.log("department:",department);
-//arrow function
-const displayStudent = () => {
-    console.log(`student Name: ${name}`);
-    };
+ const student ={
+    name : "Arjun",
+    age : 20;
+    department :CS
+ }
+ //Destructing 
+ const{name,age,department} = student;
+console.log("Name":", name);
+    console.log("Age:, age);
+        console.log("Department:", department);
 
+        //Arrow Function
+        const displaystudent =()=> {
+            console.log('student Name: ${Name}');
+       }]
+displaystudent();
+//ARRAYS 
+cont student =[
+    "Arun",
+    "priya",
+    "Devadharshini"
+
+];
+console.log(students);
+//spread operator
+const studentupdatestudent = [
+    ..students,
+];
+console.log("updatestudent");
+ // rest operator
+ function maximummarks(..marks){
+    let max= Math.max(...marks);
+    return max;
+
+ }
+ console.log("maximum marks",maximummarks(80,70,90));
